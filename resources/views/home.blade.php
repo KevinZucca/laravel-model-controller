184cd30a3e2c092145914ae2587f50ce309bb060
KevinZucca <kev.94@live.it>
2023-05-10 16:18:57 +0200

    feat: descriptions

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,9 +7,9 @@
             <div class="card-body">
               <h5 class="card-title">{{$singleMovie->title}}</h5>
               <h6 class="card-subtitle mb-2 text-body-secondary">{{$singleMovie->original_title}}</h6>
-              <p class="card-text">{{$singleMovie->nationality}}</p>
-              <p>{{$singleMovie->date}}</p>
-              <p>{{$singleMovie->vote}}</p>
+              <p class="card-text">Nationality: {{$singleMovie->nationality}}</p>
+              <p>Date: {{$singleMovie->date}}</p>
+              <p>Vote: {{$singleMovie->vote}}</p>
             </div>
         </div>
         @endforeach
